1cf9e353a6454f836b0c21ef56f43c820255662c
saytohelloworld <say.helloworld@yandex.com>
2022-08-13 03:46:49 +0000

    4day while和dowhile语句

--- c/s1e13.c
+++ c/s1e13.c
@@ -5,10 +5,67 @@ int main()
     /*while(1)
             ;*/
 
-    char ch;
+    /*char ch;
 
-    /*while ((ch = getchar()) == ' ')
+    while ((ch = getchar()) == ' ')
             ;*/
+
+    /*
+    int num = 0;
+    int ch;
+
+    printf("请输入一行英文句子：");
+    while((ch = getchar()) != '\n')
+    {
+        if (ch >= 65 && ch <= 90)
+        {
+            num = num + 1;
+        }
+    }
+
+    printf("你总共输入了%d个大写字母\n", num);
+    */
+
+    /*
+    int ch;
+
+    printf("请输入一行英文句子：");
+    while ((ch = getchar()) != '\n')
+    {
+        if (ch >= 'A' && ch <= 'Z')
+        {
+            ch = ch + 32;
+        }
+        else if (ch >= 'a' && ch <= 'z')
+        {
+            ch = ch - 32;
+        }
+        putchar(ch);
+    }
+
+    putchar('\n');
+    */
+
+    int ch, i = 0;
+
+    printf("请输入待转型的字符串：");
+    do
+    {
+        ch = getchar();
+
+        if (ch >= '0' && ch <= '9')
+        {
+            // 不懂为什么 10 * 1
+            i = 10 * i + (ch - '0');
+        }
+        else if (i)
+        {
+            break;
+        }
+    }
+    while(ch != '\n');
+
+    printf("结果是：%d\n", i);
 
     return 0;
 }
