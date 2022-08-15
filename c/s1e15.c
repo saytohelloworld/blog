d1dea53e2e540c38c003716e870432d316f0a779
saytohelloworld <say.helloworld@yandex.com>
2022-08-15 13:38:51 +0000

    6day break和contunue语句（未完成）

---
+++ c/s1e15.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    int ch;
+    int a, e, i, o, u;
+
+    a = 0;
+    e = 0;
+    i = 0;
+    o = 0;
+    u = 0;
+
+    printf("请输入一个英文句子：");
+    
+    while((ch = getchar()) != '\n')
+    {
+        switch (ch)
+        {
+            case 'a':
+            case 'A':   a++;
+                        break;
+            case 'e':
+            case 'E':   e++;
+                        break;
+            case 'i':
+            case 'I':   i++;
+                        break;
+            case 'o':
+            case 'O':   o++;
+                        break;
+            case 'u':
+            case 'U':   u++;
+                        break;
+        }
+    }
+
+    printf("您输入的句子中，包含元音字母%d个！\n", a + e + i + o + u);
+    printf("其中：a(%d),e(%d),i(%d),o(%d),u(%d)\n", a, e, i, o, u);
+    */
+
+    /*
+    int count = 2;
+    int i, j;
+    _Bool flag = 1;
+
+    for (i = 5; i <= 10000; i++)
+    {
+        for (j = 2; j < i / 2; j++)
+        {
+            if (i % j == 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag)
+        {
+            count++;
+        }
+        else
+        {
+            flag = 1;
+        }
+    }
+
+    printf("10000以内有%d个素数！\n", count);
+    */
+
+    // int offset = 3;
+
+    return 0;
+}
